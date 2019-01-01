2cc885b78c38b3ace79480208b0ed32253c3415c
Kami Koroshi <lucasvdlaan@gmail.com>
2019-01-01 19:30:57 +0100

    Made JAR compatible
    It can now run as a JAR file.
    Read the readme if you get errors.

--- src/quiz/Controller.java
+++ src/quiz/Controller.java
@@ -1,4 +1,12 @@
 package quiz;
 
+import com.shellucas.data.WorldData;
+import javafx.fxml.FXML;
+
 public class Controller {
+
+    public Controller() {
+        WorldData data = new WorldData();
+    }
+
 }
--- src/quiz/Main.java
+++ src/quiz/Main.java
@@ -1,10 +1,16 @@
 package quiz;
 
+import com.shellucas.data.utils.Continent;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.stage.Stage;
+
+import java.io.*;
+import java.net.URL;
+import java.nio.file.*;
 
 public class Main extends Application {
 
@@ -18,6 +24,41 @@ public class Main extends Application {
 
 
     public static void main(String[] args) {
+//        try {
+//            extractFiles();
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
         launch(args);
     }
+
+//    private static boolean copy(InputStream source , String destination) {
+//        if (new File(destination).exists()) return true;
+//
+//        try {
+//            Files.copy(source, Paths.get(destination), StandardCopyOption.REPLACE_EXISTING);
+//        } catch (IOException ex) {
+//            return false;
+//        }
+//
+//        return true;
+//
+//    }
+
+//    private static void extractFiles() throws IOException {
+//        File destination = new File(FileSystems.getDefault().getPath("capitals").toAbsolutePath().toString());
+//        if (!destination.mkdir()) {
+//            if (!destination.isDirectory()) {
+//                System.out.println("There is a file with the same name.");
+//            }
+//        }
+//
+//        for (Continent continent : Continent.values()) {
+//            URL inputUrl = Main.class.getResource("/capitals/capitals_" + continent.name().toLowerCase());
+//            if (!copy(inputUrl.openStream(), destination.getAbsolutePath() + "/capitals_" + continent.name().toLowerCase())) {
+//                System.out.println("File could not be extracted");
+//                System.exit(-1);
+//            }
+//        }
+//    }
 }
